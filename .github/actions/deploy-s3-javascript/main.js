a0000e2b765e49d4f8d9ae1ac80f723cedb93ec6
bobevans3 <bobevans3@gmail.com>
2023-06-03 14:15:50 -0500

    added s3 bucket deploy

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,18 @@
 const core = require('@actions/core');
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello from my custom js action')
+    // 1) Get some input values
+    const bucket =  core.getInput('bucket',{ required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+    core.notice('Hello from my custom js action');
+
+    // 2) Upload Files
+    const s3Uri = `S3://${bucket}`;
+
+    exec.exec(`aws s3 sync $(distFolder) ${s3Uri} --region ${bucketRegion}`);
 }
 
 run();
