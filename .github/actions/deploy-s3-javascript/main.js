0e1a2c40aa0f96d8318eb386880524391a9c0677
bobevans3 <bobevans3@gmail.com>
2023-06-05 04:55:33 -0500

    added s3 bucket debug 9

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,10 +11,11 @@ function run() {
     core.notice('bucket: ' + bucket);
     core.notice('bucketRegion: ' + bucketRegion);
     core.notice('distFolder: ' + distFolder);
-    core.notice('command: ' + 'aws s3 sync ${distFolder} ${s3Uri} --debug')
 
     // 2) Upload Files
     const s3Uri = `S3://${bucket}`;
+
+    core.notice('command: ' + 'aws s3 sync ' + distFolder + s3Uri + '--debug')
 
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --debug`); // --region ${bucketRegion}`);
 }
